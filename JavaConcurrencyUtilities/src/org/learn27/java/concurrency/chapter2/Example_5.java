2fae15cde23c71fba38d1aa28acc31c11289b6a9
aganesan94 <aganesan94@gmail.com>
2015-12-10 11:13:37 -0500

    added synchronized keyword to the method

--- JavaConcurrencyUtilities/src/org/learn27/java/concurrency/chapter2/Example_5.java
+++ JavaConcurrencyUtilities/src/org/learn27/java/concurrency/chapter2/Example_5.java
@@ -2,15 +2,14 @@ package org.learn27.java.concurrency.chapter2;
 
 public class Example_5 {
 
-	// adding volatile does not fix the issue.
-	private volatile int counter = 0;
+	private int counter = 0;
 
 	public static void main(String args[]) {
 		Example_5 example3 = new Example_5();
 		example3.demoInterleaving();
 	}
 
-	public void increment() {
+	public synchronized void increment() {
 		counter++;
 	}
 
